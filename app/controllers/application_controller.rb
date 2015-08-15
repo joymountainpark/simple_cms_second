e5a7f32f4d825c992ad129a7dbcbadc87e04da61
Daniel Park <joymountainpark@gmail.com>
2015-08-15 15:28:14 +0900

    Added AdminUser Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  private
+
+  def confirm_logged_in
+    unless session[:user_id]
+      flash[:notice] = "Please log in."
+      redirect_to(:controller => 'access', :action => 'login')
+      return false # halts the before_action
+    else
+      return true
+    end
+  end
+
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -39,6 +39,16 @@ class AdminUser < ActiveRecord::Base
   validate :username_is_allowed
   #validate :no_new_users_on_saturday, :on => :create
 
+  scope :sorted, lambda { order("last_name ASC, first_name ASC") }
+
+  def name
+    "#{first_name} #{last_name}"
+    # Or: first_name + ' ' + last_name
+    # Or: [first_name, last_name].join(' ')
+  end
+
+  private
+
   def username_is_allowed
     if FORBIDDEN_USERNAMES.include?(username)
       errors.add(:username, "has been restricted from use.")
